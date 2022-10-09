3866266034e29d67b44be90f4590f94a9388004d
Worlako Dzokoto <worlako1986@hotmail.com>
2022-10-09 09:37:25 +0000

    Update test case

--- legalpolicy/tests.py
+++ legalpolicy/tests.py
@@ -27,6 +27,22 @@ class LegalPolicyTestCase(TestCase):
             }
 
 
+        self.update_app_data = {
+            "platform_type": "App",
+            "app_or_website_or_service_name": "Sample 2",
+            "app_or_website_or_service_url": "https://play.google.com/store/apps/details?id=com.dowelllicenses.policies",
+            "description": "App Update",
+            "company_name": "Dowell",
+            "company_address": "Singapore 7845 STREET",
+            "company_registration_number": "SAMPLE78545REG",
+            "company_country": "Singapore",
+            "contact_email_id": "app@app.com",
+            "website_contact_page_url": "http://appsample.com",
+            "last_update_date": "2022-10-09"
+            }
+
+
+
 
 
     def test_get_legalpolicies(self):
@@ -37,5 +53,29 @@ class LegalPolicyTestCase(TestCase):
     def test_add_new_app_record(self):
         res = self.client.post(f"{self.base_url}legalpolicies/", json=self.new_app_data, headers= self.headers)
         self.assertEqual(res.status_code, 201)
+
+
+    def test_get_detail_for_legalpolicy(self):
+        res = self.client.get(f"{self.base_url}legalpolicies/FB1010000000166530784452123773/")
+        self.assertEqual(res.status_code, 200)
+
+
+
+    def test_search_detail_for_legalpolicy_using_app_or_website_name(self):
+        res = self.client.get(
+            f"{self.base_url}legalpolicies/?action_type=search&search_term=Sample")
+
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_update_app_or_website_record(self):
+        res = self.client.put(
+            f"{self.base_url}legalpolicies/FB1010000000166530784452123773/",
+            json=self.update_app_data, headers= self.headers)
+
+        self.assertEqual(res.status_code, 200)
+
+
+
 
 
